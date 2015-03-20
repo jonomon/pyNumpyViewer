ca10b130c260afa0dc496b402e19cf0bd78dffb6
jonomon <jono.chung@gmail.com>
2015-03-20 12:28:19 -0400

    Added pyNumpyViewer

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyNumpyViewer',
+      version='1.0',
+      description='View numpy arrays in the webbrowser',
+      author='Jonathan CHung',
+      author_email='jono.chung@gmail.com',
+      url='https://www.github.com/jonomon/pynumpyviewer',
+      packages=[],
+     )
